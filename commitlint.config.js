21eaddb340a883bc63d1f86ed305089756d7665d
ChaeHo95 <chlcogh11@gmail.com>
2024-10-03 22:22:40 +0900

    init: 커밋린트 설정 수정

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,22 +1,26 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ['@commitlint/config-conventional'],  // Conventional Commits 규칙 사용
   rules: {
     'type-enum': [
-      2, // 규칙을 'error'로 처리
-      'always',
+      2,  // 규칙을 '에러'로 설정하여 잘못된 타입 사용 시 에러 발생
+      'always',  // 항상 아래 타입들만 허용
       [
         'feat',        // 새로운 기능 추가
         'fix',         // 버그 수정
         'docs',        // 문서 수정
-        'style',       // 코드 스타일 변경 (세미콜론 등)
+        'design',      // 디자인 변경 (UI/UX 변경)
         'refactor',    // 리팩토링 (기능 변화 없음)
         'test',        // 테스트 코드 추가
-        'chore',       // 빌드 또는 패키지 매니저 설정
+        'chore',       // 유지보수 작업
         'build',       // 빌드 관련 코드 수정
         'ci',          // CI 관련 설정
         'perf',        // 성능 개선
         'revert',      // 이전 커밋 되돌리기
         'hotfix',      // 긴급 버그 수정
+        'init',        // 프로젝트 초기 설정 작업
+        'comment',     // 주석 추가 또는 수정
+        'deps',        // 의존성 추가, 삭제, 업데이트
+        'remove',      // 파일/폴더 삭제
       ],
     ],
   },
