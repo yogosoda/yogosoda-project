526f6cf983b4e7b8efdca6f17afe925ccae9e1f6
Yongho Kim <kyhcraft@gmail.com>
2024-10-10 10:52:28 +0900

    test: git push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Registration from '@dev/views/registration/page';
+
 export default function Home() {
-    return <div>Home</div>;
+    return (
+        <>
+            <div>Home</div>
+            <Registration />
+        </>
+    );
 }
