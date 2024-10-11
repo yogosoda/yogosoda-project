da132a7c0786af17623a50d8415bbaf1cbb308ce
ChaeHo95 <chlcogh11@gmail.com>
2024-10-11 19:06:57 +0900

    feat: 페이지 이동 버튼 구현

--- src/shared/components/PageMove.tsx
+++ src/shared/components/PageMove.tsx
@@ -13,7 +13,7 @@ const PageMove = ({
         <Link
             href={path}
             className={twMerge(
-                `w-fit bg-white text-[13px] border pl-[10px] pr-[10px] pt-[5px] pb-[5px] whitespace-nowrap`,
+                `w-fit bg-white text-[13px] border pl-[10px] pr-[10px] pt-[7px] pb-[7px] whitespace-nowrap`,
                 className
             )}
             {...props}
