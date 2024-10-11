2314beb31610455cb1584d43619e04fd18c78dda
ChaeHo95 <chlcogh11@gmail.com>
2024-10-11 19:04:53 +0900

    design: 최대 크기 지정

--- src/app/(yogo)/layout.tsx
+++ src/app/(yogo)/layout.tsx
@@ -1,6 +1,6 @@
-import { ReactNode } from 'react';
-import Header from '@dev/widgets/Header';
-import Footer from '@dev/widgets/Footer';
+import React, { ReactNode } from 'react';
+import Header from '@dev/widgets/header/Header';
+import Footer from '@dev/widgets/footer/Footer';
 
 export default function Layout({
     children,
@@ -8,11 +8,10 @@ export default function Layout({
     return (
         <div
             className={
-                'min-w-[390px] max-w-[390px]  min-h-screen flex flex-col items-center'
+                'max-w-[390px] max-w-[390px]  min-h-screen flex flex-col items-center relative'
             }
         >
             <Header />
-
             {children}
             <Footer />
         </div>
