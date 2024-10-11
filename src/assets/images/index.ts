68d60e48ce24128ce4272ae182689c6e83d33b3d
eee88 <dmsgo0823@gmail.com>
2024-10-11 13:19:46 +0900

    feat: 슬라이더 구현

---
+++ src/assets/images/index.ts
@@ -0,0 +1,8 @@
+import story_bg from './story_bg.png';
+import slider1 from './slider1.png';
+import slider2 from './slider2.png';
+import slider3 from './slider3.png';
+import slider4 from './slider4.png';
+import slider5 from './slider5.png';
+import slider6 from './slider6.png';
+export { story_bg, slider1, slider2, slider3, slider4, slider5, slider6 };
