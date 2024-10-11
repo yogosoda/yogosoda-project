4cc9052d05dd3158f1d8a5ec525413f7f28a165c
ChaeHo95 <chlcogh11@gmail.com>
2024-10-11 17:21:37 +0900

    feat: 페이지 이동 버튼 구현

---
+++ src/shared/components/PageMove.tsx
@@ -0,0 +1,34 @@
+'use client';
+import { ComponentPropsWithoutRef } from 'react';
+import Link from 'next/link';
+import { twMerge } from 'tailwind-merge';
+
+const PageMove = ({
+    className,
+    path,
+    label,
+    ...props
+}: PageMoveButtonProps) => {
+    return (
+        <Link
+            href={path}
+            className={twMerge(
+                `w-fit bg-white text-[13px] border pl-[10px] pr-[10px] pt-[5px] pb-[5px] whitespace-nowrap`,
+                className
+            )}
+            {...props}
+        >
+            {label}
+        </Link>
+    );
+};
+
+export default PageMove;
+
+type PageMoveButtonProps = Omit<
+    ComponentPropsWithoutRef<'a'>,
+    'style' | 'children' | 'onClick' | 'onDoubleClick' | 'href'
+> & {
+    path: string;
+    label: string;
+};
