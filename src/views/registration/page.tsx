642cb2b7c3ffe21aaf2de6aae1ea6c00d40a9a07
Yongho Kim <kyhcraft@gmail.com>
2024-10-12 17:33:29 +0900

    refactor: 페이지 컴포넌트 분리

--- src/views/registration/page.tsx
+++ src/views/registration/page.tsx
@@ -1,33 +1,18 @@
-import Image from 'next/image';
-import registrationImage from '../../shared/images/registration.png';
-import RegistrationButton from '@dev/shared/components/button';
+'use client';
+import RegistrationGuide from '@dev/widgets/registration/components/RegistrationGuide';
+import RegistrationCheck from '@dev/widgets/registration/components/RegistrationCheck';
+import RegistrationUsing from '@dev/widgets/registration/components/RegistrationUsing';
+import RegistrationChange from '@dev/widgets/registration/components/RegistrationChange';
 
 export default function Registration() {
     return (
         <>
-            <div className="container mx-auto">
-                <div className="flex justify-center items-center ">
-                    <div className="flex flex-col gap-4">
-                        <p className="text-teal-500 text-3xl font-bold">
-                            요고 요금제 변경
-                        </p>
-                        <div className="flex flex-col gap-2 text-black text-4xl font-bold">
-                            <p>자급제폰 KT고객님</p>
-                            <p>알뜰한 요금제</p>
-                            <p>요고로 변경하세요.</p>
-                        </div>
-                    </div>
-                    <Image
-                        src={registrationImage}
-                        width={350}
-                        height={360}
-                        alt="image"
-                    />
-                    <RegistrationButton>
-                        핸드폰 등록 및 요금제 변경하기
-                    </RegistrationButton>
-                </div>
-            </div>
+            <section className="flex flex-col justify-center items-center">
+                <RegistrationGuide />
+                <RegistrationCheck />
+                <RegistrationUsing />
+                <RegistrationChange />
+            </section>
         </>
     );
 }
