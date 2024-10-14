d87321223e028b33d628acf7bb8df265c4b6889c
Yongho Kim <kyhcraft@gmail.com>
2024-10-14 19:57:32 +0900

    chore: 경로 설정 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,13 @@ import type {Config} from "tailwindcss";
 
 const config: Config = {
     content: [
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/shared/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/views/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/widgets/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/shared/**/*.{js,ts,jsx,tsx,mdx}",
     ],
-    theme: {
-        extend: {
-            colors: {
-                background: "var(--background)",
-                foreground: "var(--foreground)",
-            },
-        },
-    },
+    theme: {},
     plugins: [],
 };
 export default config;
