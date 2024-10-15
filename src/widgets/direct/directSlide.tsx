25a78e551dc8793b4b131e576fa9f00860a9de2f
yeeunrim <1515eun@daum.net>
2024-10-15 22:28:32 +0900

    refactor: 이미지 파일 불러오기 방식을 index.ts로 분리하여 개선

--- src/widgets/direct/directSlide.tsx
+++ src/widgets/direct/directSlide.tsx
@@ -2,35 +2,8 @@
 
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
-
-import yogoSlideImage1 from '@dev/shared/images/directImage/yogo_slide_image1.png';
-import yogoSlideImage2 from '@dev/shared/images/directImage/yogo_slide_image2.png';
-import yogoSlideImage3 from '@dev/shared/images/directImage/yogo_slide_image3.png';
-import yogoSlideImage4 from '@dev/shared/images/directImage/yogo_slide_image4.png';
-import yogoSlideImage5 from '@dev/shared/images/directImage/yogo_slide_image5.png';
-import yogoSlideImage6 from '@dev/shared/images/directImage/yogo_slide_image6.png';
-import yogoSlideImage7 from '@dev/shared/images/directImage/yogo_slide_image7.png';
-import yogoSlideImage8 from '@dev/shared/images/directImage/yogo_slide_image8.png';
-import yogoSlideImage9 from '@dev/shared/images/directImage/yogo_slide_image9.png';
-import yogoSlideImage10 from '@dev/shared/images/directImage/yogo_slide_image10.png';
-import yogoSlideImage11 from '@dev/shared/images/directImage/yogo_slide_image11.png';
-import yogoSlideImage12 from '@dev/shared/images/directImage/yogo_slide_image12.png';
+import { slideImages } from '@dev/shared/images/directImage';
 import SlideControls from '@dev/shared/components/slideControls';
-
-const slideImages = [
-    yogoSlideImage1,
-    yogoSlideImage2,
-    yogoSlideImage3,
-    yogoSlideImage4,
-    yogoSlideImage5,
-    yogoSlideImage6,
-    yogoSlideImage7,
-    yogoSlideImage8,
-    yogoSlideImage9,
-    yogoSlideImage10,
-    yogoSlideImage11,
-    yogoSlideImage12,
-];
 
 export default function DirectSlide() {
     const [currentIndex, setCurrentIndex] = useState<number>(0);
@@ -56,7 +29,7 @@ export default function DirectSlide() {
     useEffect(() => {
         const interval = setInterval(() => {
             nextSlide();
-        }, 3000); // 1초마다 전환
+        }, 3000); // 3초마다 전환
 
         return () => clearInterval(interval); // 컴포넌트가 언마운트될 때 클리어
     }, []);
