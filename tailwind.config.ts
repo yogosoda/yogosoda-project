ddb06f4db1f7aa9acf1a5ae0966e17fd37c13f42
yeeunrim <1515eun@daum.net>
2024-10-15 22:34:00 +0900

    chore: tailwind.config.ts에 공유 컴포넌트 경로 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type {Config} from "tailwindcss";
 
 const config: Config = {
     content: [
+        "./src/shared/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/views/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/widgets/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
