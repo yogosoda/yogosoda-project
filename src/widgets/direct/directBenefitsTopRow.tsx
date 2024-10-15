2a868f1ec0b4895fde382127045eaf0232d9adef
yeeunrim <1515eun@daum.net>
2024-10-15 22:39:42 +0900

    refactor: text prop 대신 children 사용하도록 변경 - 주석 제거

--- src/widgets/direct/directBenefitsTopRow.tsx
+++ src/widgets/direct/directBenefitsTopRow.tsx
@@ -14,7 +14,6 @@ export default function DirectBenefitsTopRow({
     return (
         <div className="flex items-center gap-2 w-[270px] mx-auto py-2 border-t border-[#DADADA]">
             <Image src={imageSrc} alt={imageAlt} width={55} height={35} />
-            {/* props로 text를 넘기지 말고 children을 넘기고 태그 안에 text를 작성하는 것은 어떨까요? */}
             <div className="text-[12px] text-left">{children}</div>
         </div>
     );
