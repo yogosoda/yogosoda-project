dd39bb9c9905ff9d18e042a6b2940f00a7d8b5d4
Yongho Kim <kyhcraft@gmail.com>
2024-10-15 23:11:11 +0900

    refactor: 버튼 컴포넌트 ...props 추가

--- src/shared/components/button.tsx
+++ src/shared/components/button.tsx
@@ -1,6 +1,10 @@
 import { twMerge } from 'tailwind-merge';
 
-export default function RegistrationButton({ children, className }: BtnProps) {
+export default function RegistrationButton({
+    children,
+    className,
+    ...props
+}: BtnProps) {
     return (
         <>
             <button
@@ -8,6 +12,7 @@ export default function RegistrationButton({ children, className }: BtnProps) {
                     'w-80 h-11 rounded-lg bg-black text-white flex items-center gap-2 justify-center',
                     className
                 )}
+                {...props}
             >
                 {children}
             </button>
