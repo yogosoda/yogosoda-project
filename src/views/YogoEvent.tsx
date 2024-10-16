7a3e764d8a3470093495511cfd0339c4c61c25d4
ChaeHo95 <chlcogh11@gmail.com>
2024-10-16 19:54:19 +0900

    refactor: api 경로 수정

--- src/views/YogoEvent.tsx
+++ src/views/YogoEvent.tsx
@@ -1,5 +1,5 @@
 import { KTPlansType } from '@dev/entities/kt_plans.type';
-import api from '@dev/shared/utils/api';
+import api from '@devShared/utils/api';
 import YogoeventBanner from '@dev/widgets/yogoevent/YogoeventBanner';
 import YogoeventIntro from '@dev/widgets/yogoevent/YogoeventIntro';
 import YogoeventSearch from '@dev/widgets/yogoevent/YogoeventSearch';
@@ -17,13 +17,15 @@ async function YogoEvent() {
         </div>
     );
 }
+
 // [FIXME] KT 요고 요금제를 받아와 가입 혜택 페이지로 전달합니다
 const getKTPlan = async () => {
-    const res = await api.post(`${process.env.NEXT_PUBLIC_API_KT_URL}`, {
-        searchKeyword: '',
-    });
+    const res = await api.get<KTPlansType[]>(`/api/yogos`);
     const data = await res.data;
-    return data as KTPlansType[];
+    if (!data) {
+        return [];
+    }
+    return data;
 };
 
 export default YogoEvent;
