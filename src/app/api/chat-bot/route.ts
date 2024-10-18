3a42d3047ab3dedd46ac1f3c34910853d73edcf1
ChaeHo95 <chlcogh11@gmail.com>
2024-10-18 15:10:29 +0900

    refactor: KT 요금제 들고 오는 로직 수정

--- src/app/api/chat-bot/route.ts
+++ src/app/api/chat-bot/route.ts
@@ -35,13 +35,9 @@ export const POST = async (req: Request) => {
         let planDescription: PlanMeta | undefined;
 
         try {
-            const searchKeyword = '요고';
-
             const q = query(
                 collection(firestore, 'plans'),
-                where('name', '>=', searchKeyword),
-                where('name', '<=', searchKeyword + '\uf8ff'), // 요고로 시작하는 단어 포함
-                where('mvno', '==', 'KT'), // 검색 조건 추가
+                where('mvno', '==', 'KT'),
                 orderBy('fee', 'asc')
             );
             const querySnapshot = await getDocs(q);
