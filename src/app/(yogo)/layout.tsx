0fd066523cd508199c65fbf0b9917d4ba4fad340
yeeunrim <1515eun@daum.net>
2024-10-19 15:03:43 +0900

    feat: layout.tsx에서 header, footer 선택적 제시

--- src/app/(yogo)/layout.tsx
+++ src/app/(yogo)/layout.tsx
@@ -1,19 +1,24 @@
+'use client';
 import React, { ReactNode } from 'react';
+import { usePathname } from 'next/navigation';
 import Header from '@devWidgets/header/Header';
 import Footer from '@devWidgets/footer/Footer';
 
 export default function Layout({
     children,
 }: Readonly<{ children: ReactNode }>) {
+    const pathname = usePathname();
+
+    const isSplashPage = pathname === '/splash';
     return (
         <div
             className={
                 'max-w-[390px]  min-h-screen flex flex-col items-center relative'
             }
         >
-            <Header />
+            {!isSplashPage && <Header />}
             <main className="relative">{children}</main>
-            <Footer />
+            {!isSplashPage && <Footer />}
         </div>
     );
 }
