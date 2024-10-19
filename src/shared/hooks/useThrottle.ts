fdba5770eb35c195a4de488baadb382ed1b29862
ChaeHo95 <chlcogh11@gmail.com>
2024-10-19 19:21:38 +0900

    refactor: API 호출에 맞게 로직 수정

--- src/shared/hooks/useThrottle.ts
+++ src/shared/hooks/useThrottle.ts
@@ -1,31 +1,20 @@
 import { useCallback, useState } from 'react';
-import throttle from '@devShared/utils/throttle';
 
-export const useThrottle = ({
-    fn,
-    wait = 500,
-    dependence = [],
-}: ThrottleTypes) => {
+export const useThrottle = ({ fn, dependence = [] }: ThrottleTypes) => {
     // 상태 관리 (선택 사항, 필요하다면 사용할 수 있음)
     const [state, setState] = useState(false);
-    const event = useCallback(
-        throttle({
-            fn: (...args: unknown[]) => {
-                // 실제로 호출된 함수가 실행될 때 상태 변경
-                setState(true);
-                fn(...args);
-                setTimeout(() => setState(false), wait); // wait 시간 후에 상태를 복구
-            },
-            wait,
-        }),
-        dependence
-    );
+
+    const event = useCallback(async (...args: unknown[]) => {
+        setState(true);
+        await fn(...args);
+        setState(false);
+    }, dependence);
 
     return { event, state };
 };
 
 type ThrottleTypes = {
-    fn: (..._args: unknown[]) => void; // 더 구체적인 함수 타입 정의;
+    fn: (..._args: unknown[]) => Promise<void>; // 더 구체적인 함수 타입 정의;
     wait?: number;
     dependence?: unknown[];
 };
