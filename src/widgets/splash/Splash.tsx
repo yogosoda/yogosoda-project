55492fdd226084395e3dc437910af338cc6eb213
yeeunrim <1515eun@daum.net>
2024-10-21 16:38:23 +0900

    remove: splashClose.tsx 컴포넌트 삭제

--- src/widgets/splash/Splash.tsx
+++ src/widgets/splash/Splash.tsx
@@ -1,11 +1,9 @@
-import SplashClose from './components/SplashClose';
 import SplashTitle from './components/SplashTitle';
 import SplashButton from './components/SplashButton';
 
 export default function Splash() {
     return (
         <div className="h-[100vh] bg-[#FEFEF6] pb-[10px] relative overflow-hidden">
-            <SplashClose />
             <SplashTitle />
             <SplashButton />
         </div>
--- src/widgets/splash/components/SplashClose.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-
-export default function SplashClose() {
-    return (
-        <Link href="/landing">
-            <button className="text-sm float-right p-[20px]">닫기</button>
-        </Link>
-    );
-}
