8c219d977986a4136cc58a16e520f2905e70c347
yeeunrim <1515eun@daum.net>
2024-10-21 15:34:50 +0900

    feat: landing 페이지 메뉴 추가

--- src/widgets/landing/Landing.tsx
+++ src/widgets/landing/Landing.tsx
@@ -1,11 +1,13 @@
 import LandingImage from './components/LandingImage';
+import LandingMenu from './components/LandingMenu';
 import LandingTitle from './components/LandingTitle';
 
 export default function Landing() {
     return (
-        <div className="h-[100vh] bg-[#F2FEFC] pb-[10px] relative overflow-hidden">
+        <div className="h-fit bg-[#F2FEFC] pb-[10px] relative overflow-hidden">
             <LandingTitle />
             <LandingImage />
+            <LandingMenu />
         </div>
     );
 }
