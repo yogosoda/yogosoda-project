1bc0832539a0ee1ea300f7184ea46747d92cd963
yeeunrim <1515eun@daum.net>
2024-10-21 14:16:27 +0900

    feat: landing 페이지 타이틀 컴포넌트 수정

--- src/widgets/landing/components/LandingTitle.tsx
+++ src/widgets/landing/components/LandingTitle.tsx
@@ -1,9 +1,12 @@
 import yogoMembershipLogo from '@dev/shared/images/subscriptionImage/yogo_membership_logo.png';
+import landinglogokt from '@dev/shared/images/landing/landing_logo_kt.png';
+import chatboticonver2 from '@dev/shared/images/landing/chatbot_icon_ver2.png';
+import chatbotnotice from '@dev/shared/images/landing/chatbot_notice.png';
 import Image from 'next/image';
 
 export default function LandingTitle() {
     return (
-        <div>
+        <div className="flex justify-between items-center">
             <Image
                 src={yogoMembershipLogo}
                 alt="Yogo Membership Logo"
@@ -11,6 +14,26 @@ export default function LandingTitle() {
                 height={50}
                 className="m-[10px]"
             />
+            <div className="flex items-center gap-2 m-[10px]">
+                <Image
+                    src={chatbotnotice}
+                    alt="Chatbot Notice"
+                    width={100}
+                    height={100}
+                />
+                <Image
+                    src={chatboticonver2}
+                    alt="Chatbot Icon.ver2"
+                    width={17}
+                    height={20}
+                />
+                <Image
+                    src={landinglogokt}
+                    alt="Landing Logo KT"
+                    width={90}
+                    height={100}
+                />
+            </div>
         </div>
     );
 }
