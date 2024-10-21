43f9e8c723116eea242d5a1ce6cc36509582e5ea
yeeunrim <1515eun@daum.net>
2024-10-21 16:37:22 +0900

    feat: swiper를 button으로 변경

--- src/widgets/splash/Splash.tsx
+++ src/widgets/splash/Splash.tsx
@@ -1,13 +1,13 @@
 import SplashClose from './components/SplashClose';
-import SplashSwiper from './components/SplashSwiper';
 import SplashTitle from './components/SplashTitle';
+import SplashButton from './components/SplashButton';
 
 export default function Splash() {
     return (
         <div className="h-[100vh] bg-[#FEFEF6] pb-[10px] relative overflow-hidden">
             <SplashClose />
             <SplashTitle />
-            <SplashSwiper />
+            <SplashButton />
         </div>
     );
 }
---
+++ src/widgets/splash/components/SplashButton.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import splashArrowRight from '@dev/shared/images/splash/splash_arrow_right.png';
+import Link from 'next/link';
+
+export default function SplashButton() {
+    return (
+        <div className="w-[260px] flex justify-end items-center gap-3 mx-auto mt-[30px]">
+            <Link href="/landing">
+                <Image
+                    src={splashArrowRight}
+                    alt="splashArrowRight"
+                    width={50}
+                    height={50}
+                ></Image>
+            </Link>
+        </div>
+    );
+}
