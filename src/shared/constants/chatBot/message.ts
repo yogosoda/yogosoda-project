95ae5204bcb68feb60f0a16a959b89467fb46e07
ChaeHo95 <chlcogh11@gmail.com>
2024-10-23 16:20:09 +0900

    feat: 챗봇 디폴드 메세지 생성

---
+++ src/shared/constants/chatBot/message.ts
@@ -0,0 +1,13 @@
+export const FirstMessage = `# 안녕하세요! 👋  
+**요고봇**이에요! 😄  
+함께 즐거운 대화를 나눠봐요!
+
+상담 전에, 사용 중인 **통신사**를 선택해주세요. 📱`;
+
+export const QuestionNetwork = `## 알뜰폰을 사용하고 계신군요! 🎉  
+현재 **통신망**을 무엇을 사용하고 계신가요? 📱`;
+
+export const QuestionAffordableTelecom = `
+거의 다 왔습니다! 🚀  
+## 사용 중인 알뜰폰 **통신망**을 선택해주세요! 📱  
+어느 통신망을 사용하고 계신가요?`;
