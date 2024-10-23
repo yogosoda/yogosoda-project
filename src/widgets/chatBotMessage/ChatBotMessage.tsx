95a791f182c2edab45fb0aa71fd3a8d45315557e
ChaeHo95 <chlcogh11@gmail.com>
2024-10-23 16:25:55 +0900

    feat: 챗봇 채팅 메세지 컴포넌트 구현

---
+++ src/widgets/chatBotMessage/ChatBotMessage.tsx
@@ -0,0 +1,39 @@
+import bot from '@devShared/images/chatBot/yogo-chat-bot.svg';
+import Message from '@devWidgets/message/Message';
+
+const ChatBotMessage = ({
+    text,
+    time,
+    loading = false,
+}: ChatBotMessageProps) => {
+    return (
+        <div className={'flex-with-row justify-items-start gap-2.5 max-w-full'}>
+            <div
+                className={
+                    'item-center rounded-full w-[3.5rem] h-[3.5rem] bg-[#B3DAD7]'
+                }
+            >
+                <img src={bot} alt={'챗봇'} className={'w-3rem] h-[3rem]'} />
+            </div>
+            <div
+                className={'font-[1rem] font-neo-bd flex-with-col gap-2 flex-1'}
+            >
+                <div>요고봇</div>
+                <Message
+                    text={text}
+                    type={'model'}
+                    time={time}
+                    loading={loading}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default ChatBotMessage;
+
+type ChatBotMessageProps = {
+    text: string;
+    loading?: boolean;
+    time?: string;
+};
