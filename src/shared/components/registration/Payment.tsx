b3d4ed73e18be68f656f99e9134a0c912bc909ee
Yongho Kim <kyhcraft@gmail.com>
2024-10-23 17:19:02 +0900

    feat: 데이터 소진시 사용하는 속도 텍스트

--- src/shared/components/registration/Payment.tsx
+++ src/shared/components/registration/Payment.tsx
@@ -7,11 +7,11 @@ import { ReactComponent as Gift } from '@devShared/svg/gift.svg';
 import { ReactComponent as Question } from '@devShared/svg/Vector.svg';
 import { ReactComponent as Down } from '@devShared/svg/chevron-down-solid.svg';
 import { ReactComponent as Up } from '@devShared/svg/chevron-up-solid.svg';
-import { KTPlansType } from '@dev/entities/kt_plans.type';
+import { KTPlansALLType, KTPlansType } from '@dev/entities/kt_plans.type';
 
 export default function Payment() {
     const [payData, setPayData] = useState<KTPlansType[]>([]);
-    // const [payAllData, setPayAllData] = useState<[]>([]);
+    const [payAllData, setPayAllData] = useState<KTPlansALLType[]>([]);
     const [isToggle, setIsToggle] = useState<boolean[]>([]);
 
     const toggleHandler = (index: number) => {
@@ -33,20 +33,20 @@ export default function Payment() {
         }
     };
 
-    // const paymentFetchAllData = async () => {
-    //     try {
-    //         const res = await api.get<[]>('/api/plans/KT');
-    //         if (res.data) {
-    //             setPayAllData(res.data);
-    //         }
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // };
+    const paymentFetchAllData = async () => {
+        try {
+            const res = await api.get<[]>('/api/plans/KT');
+            if (res.data) {
+                setPayAllData(res.data);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     useEffect(() => {
         paymentFetchData();
-        // paymentFetchAllData();
+        paymentFetchAllData();
     }, []);
 
     return (
@@ -61,12 +61,36 @@ export default function Payment() {
                         <p className="text-[#ADB5BD] text-xs">
                             {plan.plan_name}
                         </p>
+
                         <div className="flex gap-2 items-center">
                             <p className="font-bold text-[1.3rem]">
                                 월 {plan.data.total_data}
                             </p>
-                            <Question />
+                            <button>
+                                <Question />
+                            </button>
+                            <div>
+                                {payAllData[index] && (
+                                    <div className="border rounded-lg p-2 text-white bg-slate-700">
+                                        {plan.data.total_data.includes(
+                                            '무제한'
+                                        ) ? (
+                                            <p className="text-[0.6rem] font-semibold">
+                                                무제한
+                                            </p>
+                                        ) : (
+                                            <p className="text-[0.6rem] font-semibold">
+                                                {
+                                                    payAllData[index]
+                                                        .speedWhenExhaustedDescription
+                                                }
+                                            </p>
+                                        )}
+                                    </div>
+                                )}
+                            </div>
                         </div>
+
                         <ul className="flex flex-col gap-2 text-[#979797] text-[0.65rem] font-semibold">
                             <div className="flex gap-2">
                                 <li>통화 {plan.calls_and_texts}</li>
@@ -78,15 +102,16 @@ export default function Payment() {
                                     {`${plan.data.shared_data_limit ? plan.data.shared_data_limit : '기본 데이터 한도내에서 사용'} `}
                                 </li>
                             </div>
+
                             <div>
                                 {`Y덤 혜택 (${plan.info} / ${plan.data.total_data.includes('무제한') ? plan.data.total_data : `${plan.data.total_data} + ${plan.data.total_data}`})`}
                             </div>
                         </ul>
+
                         <div className="flex gap-2 items-center">
                             <p className="font-semibold text-[#425AD5]">
                                 {`월 ${plan.monthly_fee}`}
                             </p>
-                            <Question />
                         </div>
 
                         <div className="flex justify-center">
