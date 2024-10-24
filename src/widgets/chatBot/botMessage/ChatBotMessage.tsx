e53f512e84508beebe05f9e05f404fd924a6a828
yeeunrim <1515eun@daum.net>
2024-10-24 13:11:35 +0900

    design: 챗봇 아이콘 이미지 크기 수정

--- src/widgets/chatBot/botMessage/ChatBotMessage.tsx
+++ src/widgets/chatBot/botMessage/ChatBotMessage.tsx
@@ -13,7 +13,7 @@ const ChatBotMessage = ({
                 'flex-with-row justify-items-start gap-2.5 max-w-full mt-7'
             }
         >
-            <img src={bot} alt={'챗봇'} className={'w-[3.5rem] h-[3.5rem]'} />
+            <img src={bot} alt={'챗봇'} className={'w-[3rem] h-[3rem]'} />
             <div
                 className={'font-[1rem] font-neo-bd flex-with-col gap-2 flex-1'}
             >
